fed55b5bd2caa963c6b64cebce0542fdac43f611
madmonkey <104393122+phillipc421@users.noreply.github.com>
2023-12-05 00:13:07 -0600

    use pools

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
-import { Client } from "pg";
+import { Client, Pool } from "pg";
 
-export const client = new Client({
+export const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
